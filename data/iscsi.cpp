a8341cbcd90f01478d8ec598fd3b3553fb6352b2
Jeroen van Iddekinge <iddekingej@lycos.com>
2016-08-11 22:41:11 +0200

    Add iscsi sources

---
+++ data/iscsi.cpp
@@ -0,0 +1,101 @@
+#include "iscsi.h"
+#include <QDir>
+#include <QDirIterator>
+
+TIScsiSession::TIScsiSession(QString p_name,QString p_connection)
+{
+	name=p_name;
+	connection=p_connection;
+}
+	
+void TIScsiSession::addDevice(TDevice* p_device)
+{
+	devices.append(p_device);
+}
+
+
+
+QString TIScsiSessionList::findConnection(const QString &p_path)
+{
+	QDir l_dir(p_path+"/device/");
+	QDirIterator l_di(l_dir);
+	QString l_filePath;
+	while(l_di.hasNext()){
+		l_di.next();
+	
+		if(l_di.fileName().startsWith("connection") && l_di.fileInfo().isDir()){
+			l_filePath=l_di.filePath();
+			printf("xa:%s \n",l_filePath.toUtf8().data());
+			QDir l_dir2(l_filePath+"/iscsi_connection/");
+			QDirIterator l_di2(l_dir2);
+			while(l_di2.hasNext()){
+				l_di2.next();
+				if(l_di2.fileName().startsWith("connection") && l_di2.fileInfo().isDir()){
+					return l_di2.filePath();
+				}
+			}
+			return nullptr;
+		}
+	}
+	return nullptr;
+}
+
+QString TIScsiSessionList::findTargetPath(const QString &p_path){
+	
+	QStringList l_list;
+	l_list.append("target*");
+	QDirIterator l_dirIterator(p_path,l_list);
+	if(l_dirIterator.hasNext()){
+		l_dirIterator.next();
+		return l_dirIterator.filePath();
+	}
+	return nullptr;
+}
+
+void TIScsiSessionList::processInfo(TDeviceList* p_list)
+{
+	QString l_sessionName;
+	QString l_conPath;	
+	QStringList l_list;
+	l_list.append("session*");
+	QDirIterator l_si(QString("/sys/class/iscsi_session"),l_list);
+	QString l_address;
+	QString l_port;
+	QString l_target;
+	TDevice *l_device;
+	while(l_si.hasNext()){
+		l_si.next();
+		l_sessionName=l_si.fileName();
+		
+			l_conPath=findConnection(l_si.filePath());
+			l_target=findTargetPath(l_si.filePath()+"/device/");
+			if(l_conPath != nullptr && l_target !=nullptr){
+				readString(l_conPath,"address",l_address);
+				readString(l_conPath,"port",l_port);
+				
+				TIScsiSession *l_session=new TIScsiSession(l_sessionName,l_address+":"+l_port);
+				sessions.append(l_session);
+				QDirIterator l_dit(l_target);
+				//Look in target for file containing ":"=>this are scsi bus number
+				//look device in devicelist and add to scsi list
+				while(l_dit.hasNext()){
+					l_dit.next();
+					if(l_dit.fileName().contains(":") && l_dit.fileInfo().isDir()){
+						l_device=p_list->getDeviceByScsiBus(l_dit.fileName());
+						if(l_device != nullptr){
+							l_device->setIscsiAddress(l_address+":"+l_port);
+							l_session->addDevice(l_device);
+							
+						}
+					}
+				}
+					
+
+			}
+		
+	}	
+}
+
+
+	
+	
---
+++ data/iscsi.h
@@ -0,0 +1,42 @@
+#ifndef  __ISCSI_H_
+#define __ISCSI_H_
+#include <QString>
+#include "data/device.h"
+#include "data/devicelist.h"
+#include "base/linklist.h"
+
+class TIScsiSession
+{
+private:
+	QString        name;
+	QString        connection;
+	QList<TDevice *> devices;
+public:
+	inline QString getSession(){ return name;}
+	inline QString getConnection(){return connection;}
+	
+	TIScsiSession(QString p_name,QString p_connection);
+	void addDevice(TDevice *p_device);
+	const QList<TDevice *> &getDevices(){ return devices;}
+};
+
+
+class TIScsiSessionList
+{
+private:
+	TLinkList<TIScsiSession> sessions;
+	
+	QString findConnection(const QString &p_path);
+	QString findTargetPath(const QString &p_path);
+
+public:
+	inline TLinkListItem<TIScsiSession> *getSessionTop()
+	{
+		return sessions.getStart();
+	}
+	
+	
+	void processInfo(TDeviceList *p_list);
+	
+}; 
+#endif
