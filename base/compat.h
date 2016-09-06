ffd73ac1161d0acb515f2dad0e6cf372f378a495
Jeroen van Iddekinge <iddekingej@lycos.con>
2016-09-06 14:15:30 +0200

    Make it compile for kde4

---
+++ base/compat.h
@@ -0,0 +1,16 @@
+#ifndef __COMPAT_H_
+#define __COMPAT_H_
+#include <QStringList>
+#ifndef  QStringLiteral
+#define QStringLiteral(p_a) (p_a)
+#endif
+
+#ifdef kde4
+operator <<(QStringList &p_list,const QString &p_string)
+{
+	p_list.push_back(p_string);
+	return p_list;
+}
+#endif
+
+#endif
